500cb036148b52770cb05b10962485dd0652eefa
Rory-Folster <61396000+Rory-Folster@users.noreply.github.com>
2020-03-03 09:57:30 +1300

    skrrrt

---
+++ randomtask.js
@@ -0,0 +1,17 @@
+var numone;
+
+var numtwo;
+
+var numthree;
+
+var numfour;
+
+numone=7;
+
+numtwo=4;
+
+numthree=8;
+
+numfour=2;
+
+console.log(numone / numtwo + numthree / numone - numfour % numfour);
