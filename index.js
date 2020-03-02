d9d045b8b73e580a1558a227b75aaef685e45b19
Rory-Folster <61396000+Rory-Folster@users.noreply.github.com>
2020-03-03 09:35:43 +1300

    maths questions w/ answers

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ number1=9;
 
 var number2 
 number2=13;
-
+// maths questions w/ answer
 console.log("What is 13-9");
 
 console.log(number2-number1);
 
+console.log("What is 13+9");
 
+console.log(number2+number1);
 
+console.log("What is 13/9");
 
+console.log(number1/number2);
+
+console.log("What is 9% of 13");
+
+console.log(number2%number1);
+
+console.log("What is 13x9");
+
+console.log(number1*number2);
