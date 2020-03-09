799d8ddd1a936a96eca5818cae41d4d5d0abf84f
unknown <jamie-crowther@live.nmit.ac.nz>
2020-03-10 09:47:56 +1300

    explained = operators with examples =, ==, ===, !=. !==. <, >, >=, <=

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//10/03/2020
+//10/03/2020 OPERATORS
 console.log( +true );  // 1, true is converted to 1 using unary plus
 
 console.log( +false ); // 0, false is converted to 0
@@ -23,18 +23,26 @@ console.log( x /= 10);
 //If value is a string, an attempt is made to convert the string to a number before performing the modulus and assignment.
 console.log( x %= 9);
 
-// Operator       Name        
-//   ==           Equal
-//   ===          Identical    // check equality of data type
-//   !=           Not equal
-//   !==          Not identical //check inequality of data type
-//   <           Less than
-//   >           Greater than
-//   >=          Greater than or equal to
-//   <=          Less than or equal to
-
     let a = 10
-    let b = 2
+    let b = 5
+//== Equal (is a equal to b ?)
+console.log( a == b );
+//Identical /Check equality of data type
+console.log( a === b );
+//Not equal (Does a NOT equal same as b?)
+console.log( a != b );
+//Not identical /Check inequality of data type
+console.log( a !== b );
+//Less Than (Is a less than b)
+console.log( a < b );
+//Greater Than (is a greater than b)
+console.log( a > b );
+//Greater Than or Equal To
+console.log( a >= b );
+//Less Than or Equal To
+console.log( a <= b );
+
+
 if (a == b) {
     console.log('same numbers')
 }
